e5ea0b1f5e99253bcf2b94578940b2f0585fe6f7
Daniel Zou <144640780+daniel-zouli@users.noreply.github.com>
2025-11-15 14:51:42 -0800

    Updated fetchCountry.js

--- src/api/fetchCountry.js
+++ src/api/fetchCountry.js
@@ -1,5 +1,19 @@
 // Function to fetch country data from API
 async function fetchCountryData(countryName) {
-  }
+ const apiUrl = `https://restcountries.com/v3/name/$(countryName)`;
+
+ const response = await fetch(apiUrl);
+    if (!response.ok) {
+        throw new Error("HTTP ERROR");
+}
+
+const data = await response.json();
+
+if(data.status == 404) {
+    throw new Error("404 Country not found");
+}
+return data[0];
+}
+
   
   export default fetchCountryData;
